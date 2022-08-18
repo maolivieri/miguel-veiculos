321ea420176df212f119614423423666a770d517
Matheus Olivieri <matheus.olivieri@yahoo.com>
2022-08-18 12:55:04 -0300

    feat: add home seach to homepage component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import type { NextPage } from "next";
 
 import { Header } from "../components/Header";
+import { HomeSearch } from "../components/HomeSearch";
 import { Layout } from "../components/Layout";
 
 const Home: NextPage = () => {
   return (
     <Layout>
       <Header />
-      <h1>AAAhhAAß</h1>
+      <HomeSearch />
     </Layout>
   );
 };
