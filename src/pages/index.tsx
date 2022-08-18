5d88febae607445678bac18a2248f8479877ea54
Matheus Olivieri <matheus.olivieri@yahoo.com>
2022-08-18 15:03:54 -0300

    feat: add our cards component to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,14 @@ import type { NextPage } from "next";
 import { Header } from "../components/Header";
 import { HomeSearch } from "../components/HomeSearch";
 import { Layout } from "../components/Layout";
+import { OurCars } from "../components/OurCars";
 
 const Home: NextPage = () => {
   return (
     <Layout>
       <Header />
       <HomeSearch />
+      <OurCars />
     </Layout>
   );
 };
