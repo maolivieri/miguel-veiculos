9e78aaa66f2d270d503781fc07c0d77c71c0bbe6
Matheus Olivieri <matheus.olivieri@yahoo.com>
2022-08-19 10:31:40 -0300

    feat: hide header when search box is not visble

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,16 +7,26 @@ import { TbBrandWhatsapp } from "react-icons/tb";
 
 import { IconButtonPrimary } from "../../design/IconButtonPrimary";
 
-export function Header() {
+interface IProps {
+  isSearchVisible: boolean;
+}
+
+export function Header({ isSearchVisible }: IProps) {
   return (
     <div className={styles.HeaderContainer}>
-      <div>
-        <Image width={129} height={48} src={whiteLogo} alt="" />
-      </div>
+      <div
+        className={`${styles.FixedWrapper} ${
+          !isSearchVisible && styles.hidden
+        }`}
+      >
+        <div>
+          <Image width={129} height={48} src={whiteLogo} alt="" />
+        </div>
 
-      <div className={styles.buttonsContainer}>
-        <IconButtonPrimary icon={<TbBrandWhatsapp size="1.6rem" />} />
-        <IconButtonPrimary icon={<BiMenuAltRight size="1.6rem" />} />
+        <div className={styles.buttonsContainer}>
+          <IconButtonPrimary icon={<TbBrandWhatsapp size="1.6rem" />} />
+          <IconButtonPrimary icon={<BiMenuAltRight size="1.6rem" />} />
+        </div>
       </div>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import type { NextPage } from "next";
+import { useState } from "react";
+
+import ReactVisibilitySensor from "react-visibility-sensor";
 
 import { Header } from "../components/Header";
 import { HomeSearch } from "../components/HomeSearch";
@@ -6,10 +9,22 @@ import { Layout } from "../components/Layout";
 import { OurCars } from "../components/OurCars";
 
 const Home: NextPage = () => {
+  const [isSearchVisible, setIsSearchVisible] = useState(true);
+
   return (
     <Layout>
-      <Header />
-      <HomeSearch />
+      <Header isSearchVisible={isSearchVisible} />
+      <ReactVisibilitySensor
+        partialVisibility
+        offset={{ top: 150 }}
+        // minTopValue={300}
+        onChange={(isVisible: boolean) => {
+          console.log(isVisible);
+          setIsSearchVisible(isVisible);
+        }}
+      >
+        <HomeSearch />
+      </ReactVisibilitySensor>
       <OurCars />
     </Layout>
   );
