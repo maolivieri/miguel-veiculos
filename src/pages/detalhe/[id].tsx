4428c825bf61e7d8eb1f5819d81963f86702e526
Matheus Olivieri <matheus.olivieri@yahoo.com>
2022-08-30 15:52:56 -0300

    feat: add car details page

---
+++ src/pages/detalhe/[id].tsx
@@ -0,0 +1,11 @@
+import { DetailsHeader } from "../../components/DetailsHeader";
+import styles from "./styles.module.scss";
+
+export default function CarDetailsPage() {
+  return (
+    <div className={styles.container}>
+      <DetailsHeader />
+      <h1>AAAA</h1>
+    </div>
+  );
+}
