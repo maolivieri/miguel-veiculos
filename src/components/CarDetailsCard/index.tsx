769a612c8cb7848ceb0b84842f03bdd37a819671
Matheus Olivieri <matheus.olivieri@yahoo.com>
2022-08-31 13:47:27 -0300

    feat: add car spec components to car details card

--- src/components/CarDetailsCard/index.tsx
+++ src/components/CarDetailsCard/index.tsx
@@ -7,6 +7,7 @@ import jeepLogo from "../../../public/assets/jeep_logo.svg";
 import { BsGear, BsCalendar4Week } from "react-icons/bs";
 import { GiPathDistance } from "react-icons/gi";
 import { ReturnButton } from "../../design/ReturnButton";
+import { CarDetailSpecCard } from "../CarDetailSpecCard";
 
 export function CarDetailsCard() {
   const car = {
@@ -19,32 +20,50 @@ export function CarDetailsCard() {
       <header>
         <ReturnButton />
       </header>
-      <div className={styles.imageWrapper}>
-        <Image width={1350} height={885} src={carImage} alt="" />
-      </div>
-      <div className={styles.carInfoWrapper}>
-        <div className={styles.brandImageWrapper}>
-          <Image width={150} height={150} src={jeepLogo} alt="" />
+      <div className={styles.imageAndDetailsWrapper}>
+        <div className={styles.imageWrapper}>
+          <Image width={1350} height={885} src={carImage} alt="" />
         </div>
-        <h3 className={styles.name}>Compass Limited</h3>
-        <p className={styles.price}>R$147.900,00</p>
-        <div className={styles.flagsWrapper}>
-          {(car.flag_left || car.flag_rigth) && (
-            <div
-              className={
-                car.flag_left && car.flag_rigth
-                  ? styles.leftFlag
-                  : car.flag_left && !car.flag_rigth
-                  ? styles.leftFlagOnly
-                  : styles.leftFlagInverted
-              }
-            >
-              {car.flag_left ? "IPVA PAGO" : "TANQUE CHEIO"}
+        <div>
+          <div className={styles.carInfoWrapper}>
+            <div className={styles.brandImageWrapper}>
+              <Image width={150} height={150} src={jeepLogo} alt="" />
             </div>
-          )}
-          {car.flag_left && car.flag_rigth && (
-            <div className={styles.rightFlag}>TANQUE CHEIO</div>
-          )}
+            <h3 className={styles.name}>Compass Limited</h3>
+            <p className={styles.price}>R$147.900,00</p>
+            <div className={styles.flagsWrapper}>
+              {(car.flag_left || car.flag_rigth) && (
+                <div
+                  className={
+                    car.flag_left && car.flag_rigth
+                      ? styles.leftFlag
+                      : car.flag_left && !car.flag_rigth
+                      ? styles.leftFlagOnly
+                      : styles.leftFlagInverted
+                  }
+                >
+                  {car.flag_left ? "IPVA PAGO" : "TANQUE CHEIO"}
+                </div>
+              )}
+              {car.flag_left && car.flag_rigth && (
+                <div className={styles.rightFlag}>TANQUE CHEIO</div>
+              )}
+            </div>
+          </div>
+          <div className={styles.specsWrapper}>
+            <CarDetailSpecCard icon={<BsGear />} title="KM" value="40.000 KM" />
+            <CarDetailSpecCard icon={<BsGear />} title="KM" value="40.000 KM" />
+            <CarDetailSpecCard icon={<BsGear />} title="KM" value="40.000 KM" />
+            <CarDetailSpecCard icon={<BsGear />} title="KM" value="40.000 KM" />
+            <CarDetailSpecCard icon={<BsGear />} title="KM" value="40.000 KM" />
+            <CarDetailSpecCard icon={<BsGear />} title="KM" value="40.000 KM" />
+            <CarDetailSpecCard icon={<BsGear />} title="KM" value="40.000 KM" />
+            <CarDetailSpecCard icon={<BsGear />} title="KM" value="40.000 KM" />
+            <CarDetailSpecCard icon={<BsGear />} title="KM" value="40.000 KM" />
+            <CarDetailSpecCard icon={<BsGear />} title="KM" value="40.000 KM" />
+            <CarDetailSpecCard icon={<BsGear />} title="KM" value="40.000 KM" />
+            <CarDetailSpecCard icon={<BsGear />} title="KM" value="40.000 KM" />
+          </div>
         </div>
       </div>
     </main>
