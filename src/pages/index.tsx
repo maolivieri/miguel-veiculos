9f1bf97de3e88a60d6f776674bd53b73a75eef04
Matheus Olivieri <matheus.olivieri@yahoo.com>
2022-08-31 15:32:20 -0300

    feat: add getStaticProps and call API to return list of cars

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import type { NextPage } from "next";
+import { gql } from "@apollo/client";
+import type { GetStaticProps, InferGetStaticPropsType, NextPage } from "next";
 import { useState } from "react";
 
 import ReactVisibilitySensor from "react-visibility-sensor";
@@ -8,9 +9,15 @@ import { HomeFloatingMenu } from "../components/HomeFloatingMenu";
 import { HomeSearch } from "../components/HomeSearch";
 import { Layout } from "../components/Layout";
 import { OurCars } from "../components/OurCars";
+import { client } from "../lib/apollo";
+import { ICar } from "../types/Car";
 
-const Home: NextPage = () => {
+const Home: NextPage = ({
+  carsProps,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   const [isSearchVisible, setIsSearchVisible] = useState(true);
+
+  const cars: ICar[] = carsProps;
 
   return (
     <Layout>
@@ -25,10 +32,76 @@ const Home: NextPage = () => {
       >
         <HomeSearch />
       </ReactVisibilitySensor>
-      <OurCars />
+      <OurCars cars={cars} />
       <HomeFloatingMenu isSearchVisible={isSearchVisible} />
     </Layout>
   );
 };
 
 export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await client.query({
+    query: gql`
+      query MyQuery {
+        cars {
+          id
+          km
+          modelo
+          opcional_direito
+          opcional_esquerdo
+          portas
+          potencia
+          preco
+          publishedAt
+          stage
+          tracao
+          marca {
+            nome
+            logo {
+              url
+            }
+          }
+          main_image {
+            url
+          }
+          ano
+          assentos
+          autonomia
+          cambio
+          carroceria
+          combustivel
+          confortos {
+            nome
+          }
+          cor
+          essenciais {
+            nome
+          }
+          fotos {
+            ... on Foto {
+              id
+              fotos {
+                url
+              }
+            }
+          }
+          segurancas {
+            nome
+          }
+          tecnologias {
+            nome
+          }
+        }
+      }
+    `,
+  });
+
+  const cars: ICar[] = data.cars;
+
+  return {
+    props: {
+      carsProps: cars,
+    },
+  };
+};
