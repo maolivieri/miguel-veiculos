8177702918f3a3fffd8069146e4304f3a5ca4b78
Matheus Olivieri <matheus.olivieri@yahoo.com>
2022-09-09 17:10:00 -0300

    style: improve styles for heigh of headers and main

--- src/pages/nossa-loja/index.tsx
+++ src/pages/nossa-loja/index.tsx
@@ -6,18 +6,22 @@ export default function OurStore() {
   return (
     <div className={styles.container}>
       <SideDrawer />
-      <SubPagesHeader />
-      <main>
-        <h1>Nossa Loja</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Turpis
-          massa sed elementum tempus egestas sed sed. Est velit egestas dui id
-          ornare arcu odio ut. Posuere morbi leo urna molestie at. Vitae auctor
-          eu augue ut lectus arcu bibendum at varius. Vitae auctor eu augue ut
-          lectus arcu bibendum at varius.
-        </p>
-        {/* <p>…</p> */}
+      <header className={styles.header}>
+        <SubPagesHeader />
+      </header>
+      <main className={styles.main}>
+        <div className={styles.card}>
+          <h1 className={styles.title}>Nossa Loja</h1>
+          <p className={styles.copy}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Turpis
+            massa sed elementum tempus egestas sed sed. Est velit egestas dui id
+            ornare arcu odio ut. Posuere morbi leo urna molestie at. Vitae
+            auctor eu augue ut lectus arcu bibendum at varius. Vitae auctor eu
+            augue ut lectus arcu bibendum at varius.
+          </p>
+          {/* <p>…</p> */}
+        </div>
       </main>
     </div>
   );
--- src/pages/sobre-nos/index.tsx
+++ src/pages/sobre-nos/index.tsx
@@ -6,15 +6,19 @@ export default function AboutUs() {
   return (
     <div className={styles.container}>
       <SideDrawer />
-      <SubPagesHeader />
-      <main>
-        <h1>Sobre Nós</h1>
-        <p>
-          Fundada por Miguel Rodrigues de Jesus, na cidade de Capivari, no
-          interior de São Paulo, a loja Miguel Veículos vem atuando no mercado
-          de venda de carros semi-novos desde 1998.
-        </p>
-        {/* <p>…</p> */}
+      <header className={styles.header}>
+        <SubPagesHeader />
+      </header>
+      <main className={styles.main}>
+        <div className={styles.card}>
+          <h1 className={styles.title}>Sobre Nós</h1>
+          <p className={styles.copy}>
+            Fundada por Miguel Rodrigues de Jesus, na cidade de Capivari, no
+            interior de São Paulo, a loja Miguel Veículos vem atuando no mercado
+            de venda de carros semi-novos desde 1998.
+          </p>
+          {/* <p>…</p> */}
+        </div>
       </main>
     </div>
   );
