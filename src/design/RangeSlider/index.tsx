82c46d703cb8d19ff272e802ae88a5e091464b44
Matheus Olivieri <matheus.olivieri@yahoo.com>
2022-09-12 15:17:16 -0300

    feat: create range slider component

--- src/design/RangeSlider/index.tsx
+++ src/design/RangeSlider/index.tsx
@@ -1,28 +1,43 @@
-import { useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import styles from "./styles.module.scss";
 
-export function RangeSlider() {
-  const [value1, setValue1] = useState(0);
-  const [value2, setValue2] = useState(0);
+interface IProps {
+  maxRange: number;
+  minRange: number;
+  minValue: number;
+  maxValue: number;
+  setMinValue: Dispatch<SetStateAction<number>>;
+  setMaxValue: Dispatch<SetStateAction<number>>;
+}
 
+export function RangeSlider({
+  maxRange,
+  maxValue,
+  minRange,
+  minValue,
+  setMaxValue,
+  setMinValue,
+}: IProps) {
   return (
     <div className={styles.SliderContainer}>
       <input
-        title='left'
-        type='range'
-        min='0'
-        max='1000'
-        aria-label='range-slider2'
-        // value={value1}
-        onChange={(e) => console.log(e.target.value)}
+        title="min"
+        aria-label="range-slider-min"
+        type="range"
+        min={minRange}
+        max={maxRange}
+        value={minValue}
+        onChange={(e) => setMinValue(Number(e.target.value))}
         className={`${styles.thumb} ${styles.thumbleft}`}
       />
       <input
-        title='rigth'
-        aria-label='range-slider2'
-        type='range'
-        min='0'
-        max='1000'
+        title="max"
+        aria-label="range-slider-max"
+        type="range"
+        min={minRange}
+        max={maxRange}
+        value={maxValue}
+        onChange={(e) => setMaxValue(Number(e.target.value))}
         className={`${styles.thumb} ${styles.thumbright}`}
       />
       <div className={styles.slider}>
