9f4a7194d96ae01000722902997391929d80fdef
Matheus Olivieri <matheus.olivieri@yahoo.com>
2022-09-12 16:03:55 -0300

    chore: rename price filter component

---
+++ src/components/FiltersModal/components/FilterPreco.tsx
@@ -0,0 +1,69 @@
+import { useContext, useState } from "react";
+import { ButtonPrimary } from "../../../design/ButtonPrimary";
+import { IconPreco } from "../../../design/Icons";
+import { RangeSlider } from "../../../design/RangeSlider";
+import styles from "./styles.module.scss";
+import { formatToCurrency } from "../../../lib/formatToCurrency";
+import { SystemContext } from "../../../context/systemContext";
+import { ButtonSecondary } from "../../../design/ButtonSecondary";
+
+export function FilterPreco() {
+  const { activeFilters, setActiveFilters, setFocusedFilter } =
+    useContext(SystemContext);
+  const minRange = 10000;
+  const maxRange = 200000;
+  const [minValue, setMinValue] = useState(activeFilters.minPrice || minRange);
+  const [maxValue, setMaxValue] = useState(activeFilters.maxPrice || maxRange);
+
+  const handleConfirmFilter = () => {
+    setActiveFilters((prevState) => ({
+      ...prevState,
+      minPrice: minValue,
+      maxPrice: maxValue,
+    }));
+    setFocusedFilter(null);
+  };
+
+  const clearPriceFilters = () => {
+    setMinValue(minRange);
+    setMaxValue(maxRange);
+    setActiveFilters((prevState) => ({
+      ...prevState,
+      minPrice: null,
+      maxPrice: null,
+    }));
+  };
+
+  return (
+    <>
+      <div className={styles.mainTitleWrapper}>
+        <IconPreco size="1.4rem" />
+        <h3>Preço</h3>
+      </div>
+      <div className={styles.filterWrapper}>
+        <div className={styles.filterRange}>
+          <div>
+            <p>De</p>
+            <h6>{formatToCurrency(minValue)}</h6>
+          </div>
+          <div>
+            <p>Até</p>
+            <h6>{formatToCurrency(maxValue)}</h6>
+          </div>
+        </div>
+        <RangeSlider
+          minRange={minRange}
+          maxRange={maxRange}
+          minValue={minValue}
+          maxValue={maxValue}
+          setMinValue={setMinValue}
+          setMaxValue={setMaxValue}
+        />
+        <div className={styles.buttonsWrapper}>
+          <ButtonPrimary text="Confirmar" onClick={handleConfirmFilter} />
+          <ButtonSecondary text="Limpar" onClick={clearPriceFilters} />
+        </div>
+      </div>
+    </>
+  );
+}
