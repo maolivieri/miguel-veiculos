377696e31cedb8b0778858069ab0f9d2fea59829
Matheus Olivieri <matheus.olivieri@yahoo.com>
2023-03-05 11:38:35 -0300

    chore: changes to about us copy text

--- src/pages/sobre-nos/index.tsx
+++ src/pages/sobre-nos/index.tsx
@@ -1,16 +1,23 @@
 import SubPages from "../../components/SubPage";
 
 export default function AboutUs() {
+  const pStyles = { lineHeight: "1.5rem", color: "var(--gray-800)" };
+
   return (
-    <SubPages
-      images={[""]}
-      copy="Fundada por Miguel Rodrigues de Jesus, na cidade de Capivari, no
-    interior de São Paulo, a loja Miguel Veículos vem atuando no
-    mercado de venda de carros semi-novos desde 1998.
-    
-    
-    "
-      title="Sobre Nós"
-    />
+    <SubPages images={[""]} title="Sobre Nós">
+      <div>
+        <p style={pStyles}>
+          Fundada por <b>Miguel Rodrigues de Jesus</b>, na cidade de Capivari,
+          interior de São Paulo, a loja <b>Miguel Veículos</b> vem atuando no
+          mercado de venda de carros semi-novos desde 1998.
+        </p>
+        <p style={pStyles}>
+          A ética, transparência e compromisso com nossos clientes sempre foram
+          prioridades em nossos negócios e os princiapis pilares que moldaram a
+          trajetória da nossa marca.
+        </p>
+        <p style={pStyles}>-.</p>
+      </div>
+    </SubPages>
   );
 }
